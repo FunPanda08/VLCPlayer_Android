5e13b83e86b5f2ebe83a5c8043ca56b0c3615daa
FunPanda08 <funnypanda.v8@gmail.com>
2014-06-15 22:52:10 +0800

    Video Player Ultimate(HD) source upload.

---
+++ src/org/videolan/vlc/gui/SidebarAdapter.java
@@ -0,0 +1,175 @@
+/*****************************************************************************
+ * SidebarAdapter.java
+ *****************************************************************************
+ * Copyright © 2012-2013 VLC authors and VideoLAN
+ * Copyright © 2012-2013 Edward Wang
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+package org.videolan.vlc.gui;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import media.hd.video.player.R;
+import org.videolan.vlc.Util;
+import org.videolan.vlc.VLCApplication;
+import org.videolan.vlc.gui.audio.AudioBrowserFragment;
+import org.videolan.vlc.gui.video.VideoGridFragment;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class SidebarAdapter extends BaseAdapter {
+    public final static String TAG = "VLC/SidebarAdapter";
+
+    static class SidebarEntry {
+        String id;
+        String name;
+        int attributeID;
+
+        public SidebarEntry(String _id, int _name, int _attributeID) {
+            this.id = _id;
+            this.name = VLCApplication.getAppContext().getString(_name);
+            this.attributeID = _attributeID;
+        }
+    }
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+    static final List<SidebarEntry> entries;
+    private HashMap<String, Fragment> mFragments;
+    private String mCurrentFragmentId;
+
+    static {
+        SidebarEntry entries2[] = {
+            new SidebarEntry( "video", R.string.video, R.attr.ic_menu_video ),
+            new SidebarEntry( "audio", R.string.audio, R.attr.ic_menu_audio ),
+            new SidebarEntry( "directories", R.string.directories, R.attr.ic_menu_folder ),
+            new SidebarEntry( "history", R.string.history, R.attr.ic_menu_history ),
+            //new SidebarEntry( "bookmarks", R.string.bookmarks, R.drawable.ic_bookmarks ),
+            //new SidebarEntry( "playlists", R.string.playlists, R.drawable.icon ),
+        };
+        entries = Arrays.asList(entries2);
+    }
+
+    public SidebarAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mFragments = new HashMap<String, Fragment>(entries.size());
+    }
+
+    @Override
+    public int getCount() {
+        return entries.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return entries.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return (long)position; // The SidebarEntry list is unique
+    }
+
+    @Override
+    public View getView(int position, View v, ViewGroup parent) {
+        SidebarEntry sidebarEntry = entries.get(position);
+
+        /* If view not created */
+        if(v == null) {
+            v = mInflater.inflate(R.layout.sidebar_item, parent, false);
+        }
+        TextView textView = (TextView)v;
+        textView.setText(sidebarEntry.name);
+        Drawable img = VLCApplication.getAppResources().getDrawable(
+                Util.getResourceFromAttribute(mContext, sidebarEntry.attributeID));
+        if (img != null) {
+            int dp_32 = Util.convertDpToPx(32);
+            img.setBounds(0, 0, dp_32, dp_32);
+            textView.setCompoundDrawables(img, null, null, null);
+        }
+        // Set in bold the current item.
+        if (mCurrentFragmentId.equals(sidebarEntry.id))
+            textView.setTypeface(null, Typeface.BOLD);
+        else
+            textView.setTypeface(null, Typeface.NORMAL);
+
+        return v;
+    }
+
+    public Fragment fetchFragment(String id) {
+        // Save the previous fragment in case an error happens after.
+        String prevFragmentId = mCurrentFragmentId;
+
+        // Set the current fragment.
+        setCurrentFragment(id);
+
+        if(mFragments.containsKey(id) && mFragments.get(id) != null) {
+            return mFragments.get(id);
+        }
+        Fragment f;
+        if(id.equals("audio")) {
+            f = new AudioBrowserFragment();
+        } else if(id.equals("video")) {
+            f = new VideoGridFragment();
+        } else if(id.endsWith("directories")) {
+            f = new DirectoryViewFragment();
+        } else if(id.equals("history")) {
+            f = new HistoryFragment();
+        }
+        else {
+            mCurrentFragmentId = prevFragmentId; // Restore the current fragment id.
+            throw new IllegalArgumentException("Wrong fragment id.");
+        }
+        f.setRetainInstance(true);
+        mFragments.put(id, f);
+        return f;
+    }
+
+    private void setCurrentFragment(String id) {
+        mCurrentFragmentId = id;
+        this.notifyDataSetChanged();
+    }
+
+    /**
+     * When Android has automatically recreated a fragment from the bundle state,
+     * use this function to 'restore' the recreated fragment into this sidebar
+     * adapter to prevent it from trying to create the same fragment again.
+     *
+     * @param id ID of the fragment
+     * @param f The fragment itself
+     */
+    public void restoreFragment(String id, Fragment f) {
+        if(f == null) {
+            Log.e(TAG, "Can't set null fragment for " + id + "!");
+            return;
+        }
+        mFragments.put(id, f);
+        setCurrentFragment(id);
+        // if Android added it, it's been implicitly added already...
+    }
+}
---
+++ src/org/videolan/vlc/gui/audio/EqualizerFragment.java
@@ -0,0 +1,238 @@
+/*****************************************************************************
+ * EqualizerFragment.java
+ *****************************************************************************
+ * Copyright © 2013 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+package org.videolan.vlc.gui.audio;
+
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.LibVlcException;
+import media.hd.video.player.R;
+import org.videolan.vlc.Util;
+import org.videolan.vlc.VLCApplication;
+import org.videolan.vlc.interfaces.OnEqualizerBarChangeListener;
+import org.videolan.vlc.widget.EqualizerBar;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+public class EqualizerFragment extends SherlockFragment {
+
+    public final static String TAG = "VLC/EqualizerFragment";
+    private ToggleButton button;
+    private Spinner equalizer_presets;
+    private SeekBar preamp;
+    private LinearLayout bands_layout;
+    LibVLC libVlc = null;
+    float[] equalizer = null;
+
+    /* All subclasses of Fragment must include a public empty constructor. */
+    public EqualizerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        getSherlockActivity().getSupportActionBar().setTitle(getResources().getString(R.string.equalizer));
+
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.equalizer, container, false);
+        saveViewChildren(v);
+
+        return v;
+    }
+
+    private void saveViewChildren(View v) {
+        button = (ToggleButton) v.findViewById(R.id.equalizer_button);
+        equalizer_presets = (Spinner) v.findViewById(R.id.equalizer_presets);
+        preamp = (SeekBar) v.findViewById(R.id.equalizer_preamp);
+        bands_layout = (LinearLayout) v.findViewById(R.id.equalizer_bands);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.equalizer, null);
+        ViewGroup rootView = (ViewGroup) getView();
+        rootView.removeAllViews();
+        rootView.addView(v);
+        saveViewChildren(v);
+
+        fillViews();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fillViews();
+    }
+
+    private void fillViews() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(VLCApplication.getAppContext());
+        float[] bands = null;
+        String[] presets = null;
+        try {
+            libVlc = Util.getLibVlcInstance();
+            bands = libVlc.getBands();
+            presets = libVlc.getPresets();
+            if (equalizer == null)
+                equalizer = Util.getFloatArray(preferences, "equalizer_values");
+            if (equalizer == null)
+                equalizer = new float[bands.length + 1];
+        } catch (LibVlcException e) {
+            e.printStackTrace();
+        }
+
+        // on/off
+        button.setChecked(libVlc.getEqualizer() != null);
+        button.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (libVlc == null)
+                    return;
+                libVlc.setEqualizer(isChecked ? equalizer : null);
+            }
+        });
+
+        // presets
+        equalizer_presets.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, presets));
+
+        // Set the default selection asynchronously to prevent a layout initialization bug.
+        final int equalizer_preset_pref = preferences.getInt("equalizer_preset", 0);
+        equalizer_presets.post(new Runnable() {
+            @Override
+            public void run() {
+                equalizer_presets.setSelection(equalizer_preset_pref, false);
+                equalizer_presets.setOnItemSelectedListener(mPresetListener);
+            }
+        });
+
+        // preamp
+        preamp.setMax(40);
+        preamp.setProgress((int) equalizer[0] + 20);
+        preamp.setOnSeekBarChangeListener(mPreampListener);
+
+        // bands
+        for (int i = 0; i < bands.length; i++) {
+            float band = bands[i];
+
+            EqualizerBar bar = new EqualizerBar(getActivity(), band);
+            bar.setValue(equalizer[i + 1]);
+            bar.setListener(new BandListener(i + 1));
+
+            bands_layout.addView(bar);
+            LinearLayout.LayoutParams params =
+                    new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                                                  LayoutParams.MATCH_PARENT, 1);
+            bar.setLayoutParams(params);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        button.setOnCheckedChangeListener(null);
+        equalizer_presets.setOnItemSelectedListener(null);
+        preamp.setOnSeekBarChangeListener(null);
+        bands_layout.removeAllViews();
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(VLCApplication.getAppContext());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("equalizer_enabled", button.isChecked());
+        Util.putFloatArray(editor, "equalizer_values", equalizer);
+        editor.putInt("equalizer_preset", equalizer_presets.getSelectedItemPosition());
+        editor.commit();
+    }
+
+    private final OnItemSelectedListener mPresetListener = new OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (libVlc == null)
+                return;
+            float[] preset = libVlc.getPreset(pos);
+            if (preset == null)
+                return;
+
+            equalizer = preset;
+            preamp.setProgress((int) equalizer[0] + 20);
+            for (int i = 0; i < equalizer.length - 1; ++i) {
+                EqualizerBar bar = (EqualizerBar) bands_layout.getChildAt(i);
+                bar.setValue(equalizer[i + 1]);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    };
+
+    private final OnSeekBarChangeListener mPreampListener = new OnSeekBarChangeListener() {
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (!fromUser)
+                return;
+
+            equalizer[0] = progress - 20;
+            if (libVlc != null && button.isChecked())
+                libVlc.setEqualizer(equalizer);
+        }
+    };
+
+    private class BandListener implements OnEqualizerBarChangeListener {
+        private int index;
+
+        public BandListener(int index) {
+            this.index = index;
+        }
+
+        @Override
+        public void onProgressChanged(float value) {
+            equalizer[index] = value;
+            if (libVlc != null && button.isChecked())
+                libVlc.setEqualizer(equalizer);
+        }
+    };
+}
